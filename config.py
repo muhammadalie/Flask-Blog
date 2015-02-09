bac1db387abf3b9f516d683b67c4c9ff414db53d
abhilash <aaabhilash97@gmail.com>
2015-02-09 10:53:40 +0530

    1.0

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'aaabhilash97@gmail.com'
+MAIL_PASSWORD = 'passion*'
+ADMINS = ['aaabhilash97@gmail.com']
+WTF_CSRF_ENABLED=True
+SECRET_KEY='nosecret'
+SECURITY_PASSWORD_SALT='nopassword'
+SESSION_COOKIE_NAME='STUDENTAPP'
+SESSION_COOKIE_PATH='tmp/'
